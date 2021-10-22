094b5abc6752e016a70865c95ce661b0a03e8b5d
CeAlCuadrado <cealcuadrado@outlook.com>
2021-10-22 18:54:33 -0300

    Definido layout

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,7 +1,15 @@
 function Layout() {
-    return (
-        <p>Hola, mundo!</p>
-    );
+  const shootDart = (points) => {
+    alert(`You got ${points} points`);
+  };
+
+  return (
+    <div className="layout">
+      <button className="btn btn-outline me-2" onClick={() => shootDart(1)}>Shoot white</button>
+      <button className="btn btn-success me-2" onClick={() => shootDart(5)}>Shoot green</button>
+      <button className="btn btn-danger" onClick={() => shootDart(10)}>Shoot red</button>
+    </div>
+  );
 }
 
 export default Layout;
